4df68e0c2ee36d93fcfa72a3f42fe230cfe6394d
caroline gluck <gluckc@amazon.com>
2022-07-10 10:38:12 -0400

    add default region

--- python/app.py
+++ python/app.py
@@ -6,15 +6,18 @@ from api.infrastructure import APIStack
 from integration.infrastructure import IntegrationStack
 from recognition.infrastructure import RekognitionStack
 
+DEFAULT_REGION = 'us-east-2'
+
 app = cdk.App()
-apiStack = APIStack(app, "APIStack")
-integrationStack = IntegrationStack(app, "IntegrationStack")
+apiStack = APIStack(app, "APIStack", env=cdk.Environment(region=DEFAULT_REGION))
+integrationStack = IntegrationStack(app, "IntegrationStack", env=cdk.Environment(region=DEFAULT_REGION))
 RekognitionStack(
     app,
     "RekognitionStack",
     sqs_url=apiStack.sqs_url,
     sqs_arn=apiStack.sqs_arn,
     sns_arn=integrationStack.sns_arn,
+    env=cdk.Environment(region=DEFAULT_REGION)
 )
 
 app.synth()
