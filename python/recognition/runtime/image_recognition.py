1557d9f554d1421de0778166704388572476db02
Florian Mair <61224241+flo-mair@users.noreply.github.com>
2022-07-11 16:54:58 -0400

    Update image_recognition.py

--- python/recognition/runtime/image_recognition.py
+++ python/recognition/runtime/image_recognition.py
@@ -11,20 +11,47 @@ queue_url = os.environ["SQS_QUEUE_URL"]
 table_name = os.environ["TABLE_NAME"]
 topic_arn = os.environ["TOPIC_ARN"]
 
-# Detect labels from image with Rekognition
-# Save labels to DynamoDB
-# Publish item to SNS
+# Calls Amazon Rekognition DetectLabels API to classify images in S3
+def detectImgLabels(bucket_name, key, maxLabels=10, minConfidence=70):
+    image = {
+        "S3Object": {
+            "Bucket": bucket_name,
+            "Name": key
+        }
+    }
+    response = rekognition.detect_labels(Image=image, MaxLabels=10, MinConfidence=70)
+    return response
+
+
+# Write item to DynamoDB
+def writeToDynamoDb(tableName, item):
+    dynamodb.put_item(
+        TableName=tableName,
+        Item=item
+    )
+
+# Send message to SNS
+def triggerSNS(message):
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=message,
+        Subject="CodeWhisperer Workshop Success!",
+
+    )
+    print(response)
+
 # Delete message from SQS
-
-# <<Amazon CodeWhisperer generated code goes here>>
-
+def deleteFromSqs(receipt_handle):
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
 
 
 def handler(event, context):
     print(event)
-    print(type(event))
     try:
-        # 1. process message from SQS
+        # 1. Read message from SQS
         for Record in event.get("Records"):
             receipt_handle = Record.get("receiptHandle")
             for record in json.loads(Record.get("body")).get("Records"):
@@ -32,16 +59,27 @@ def handler(event, context):
                 key = record.get("s3").get("object").get("key")
 
                 # 2. Use Amazon Rekognition to recognize the image
-
+                labels = detectImgLabels(bucket_name=bucket_name, key=key)
+                print(key, labels["Labels"])
 
                 # 3. Persist result on DynamoDB
+                db_result = []
+                json_labels = json.dumps(labels["Labels"])
+                db_labels = json.loads(json_labels)
+                for label in db_labels:
+                    db_result.append(label["Name"])
+                db_item = {
+                    "image": {"S": key},
+                    "labels": {"S": str(db_result)}
+                }
 
+                writeToDynamoDb(tableName=table_name, item=db_item)
 
                 # 4. Send message to SNS
-
+                triggerSNS(str(db_result))
 
                 # 5. Delete img from SQS
-
+                deleteFromSqs(receipt_handle=receipt_handle)
 
     except Exception as e:
         print(e)
