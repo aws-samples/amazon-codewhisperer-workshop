8b869f472710dcee32286edc42a716ccfc09ca9f
Florian Mair <61224241+flo-mair@users.noreply.github.com>
2022-07-11 16:54:36 -0400

    Update list_images.py

--- python/recognition/runtime/list_images.py
+++ python/recognition/runtime/list_images.py
@@ -5,14 +5,19 @@ import json
 table_name = os.environ["TABLE_NAME"]
 attribute_name = "image_name"
 
-# Function to list all items from a DynamoDB table
+def list_items(table_name):
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(table_name)
+    response = table.scan()
+    items = response['Items']
+    return items
 
 # <<Amazon CodeWhisperer generated code goes here>>
 
 def handler(event, context):
     # 1. Scan items from DynamoDB
-
+    response = list_items(table_name)
     return {
-        #"body": json.dumps(response),
+        "body": json.dumps(response),
         "statusCode": 200
     }
