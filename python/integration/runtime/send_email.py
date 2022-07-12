dbf535811f7b21b72eef4ff7dcd32b6d23c68322
caroline gluck <gluckc@amazon.com>
2022-07-12 10:50:01 -0400

    add comments with numbers

--- python/integration/runtime/send_email.py
+++ python/integration/runtime/send_email.py
@@ -1,13 +1,19 @@
 from xml.etree.ElementTree import Element, ElementTree, tostring
+import requests
 
-# Convert JSON string to XML string
-# Send XML string with HTTP POST
+# 1.) Convert JSON string to XML string
+
+# 2.) Send XML string with HTTP POST
 
 # <<Amazon CodeWhisperer generated code goes here>>
 
 def handler(event, context):
 
+    # call method 1.) with var "event" to convert json to xml
+
+    # call method 2.) to post xml
+
     return {
         'statusCode': 200,
-        "message": "E-mail sent"
+        "message": "Success!"
     }
