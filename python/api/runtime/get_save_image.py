52d87ced86547568bd5af43ee3349a097d0c3ba1
caroline gluck <gluckc@amazon.com>
2022-07-12 10:38:37 -0400

    update fn to method in comment

--- python/api/runtime/get_save_image.py
+++ python/api/runtime/get_save_image.py
@@ -8,6 +8,7 @@ s3_client = boto3.client("s3")
 S3_BUCKET = os.getenv('BUCKET_NAME')
 
 # 1.) Function to get a file from url
+
 # 2.) Function to upload image to S3
 
 # <<Amazon CodeWhisperer generated code goes here>>
@@ -17,7 +18,7 @@ def handler(event, context):
     url = event["queryStringParameters"]["url"]
     name = event["queryStringParameters"]["name"]
 
-    # pass the output of function #1 as input to function #2
+    # pass the output of method #1 as input to method #2
 
     return {
         'statusCode': 200,
