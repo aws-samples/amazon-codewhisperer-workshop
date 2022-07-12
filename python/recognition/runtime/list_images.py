b5d6817db7f5e08cee329aa7f51e0d6227664545
caroline gluck <gluckc@amazon.com>
2022-07-12 10:39:07 -0400

    update comment numbering, remove attr name

--- python/recognition/runtime/list_images.py
+++ python/recognition/runtime/list_images.py
@@ -3,14 +3,13 @@ import boto3
 import json
 
 table_name = os.environ["TABLE_NAME"]
-attribute_name = "image_name"
 
-# Function to list all items from a DynamoDB table
+# 1.) Function to list all items from a DynamoDB table
 
 # <<Amazon CodeWhisperer generated code goes here>>
 
 def handler(event, context):
-    # 1. Scan items from DynamoDB
+    # call method 1.) to scan items from DynamoDB
 
     return {
         #"body": json.dumps(response),
